1651698525a3a1bdb48f4b2a319b07c2927579a4
Eric Charbonnier <eric2503@hotmail.com>
2023-09-17 17:50:45 -0400

    aider l'ide avec dummy fucntion

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
 
 // objet who managed the isLoggedIn state and put it to false. objet who contains components. AuthContent , i write it like that because it's an objet who contains a component
-const AuthContext = React.createContext({ 
+const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {}
+  // ci-dessous ajout d'une fonction factice/dummy, pour aider l'IDE
 });
 
 // we do not need it here but we need it in other scryRenderedComponentsWithType, therefore i export default. so now we can import this specific object
