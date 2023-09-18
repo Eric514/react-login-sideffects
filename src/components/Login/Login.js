aad776781fa29389ffe9fa97757604e0b921d5e3
Eric Charbonnier <eric2503@hotmail.com>
2023-09-18 16:45:34 -0400

    avoir un composant Input reutilisable

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -114,22 +114,7 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            // emailIsValid === false ? classes.invalid : ""
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            // value={enteredEmail}
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
+
         <div
           className={`${classes.control} ${
             // passwordIsValid === false ? classes.invalid : ""
